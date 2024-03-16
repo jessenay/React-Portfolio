3a455113865217403386c036650923bcfec02b08
Jesse Nay <jessenay26@gmail.com>
2024-03-16 12:09:50 -0600

    added a footer with links

---
+++ My-React-Portfolio/src/components/Footer.jsx
@@ -0,0 +1,21 @@
+import React from 'react';
+import { FaGithub, FaLinkedin, FaEnvelope } from 'react-icons/fa';
+
+function Footer() {
+  return (
+    <footer className="text-center my-icon">
+      <a href="https://github.com/jessenay" target="_blank" rel="noopener noreferrer">
+        <FaGithub size="2em" style={{ color: 'rgb(214, 207, 180)' }} />
+      </a>
+      <a href="https://linkedin.com/in/yourusername" className="mx-3" target="_blank" rel="noopener noreferrer">
+        <FaLinkedin size="2em" style={{ color: 'rgb(214, 207, 180)' }} /> {/* LinkedIn blue color */}
+      </a>
+      <a href="mailto:jessenay26@gmail.com">
+        <FaEnvelope size="2em" style={{ color: 'rgb(214, 207, 180)' }} />
+      </a>
+    </footer>
+  );
+}
+
+export default Footer;
+
